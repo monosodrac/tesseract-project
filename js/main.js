ee4d7148a7b1d8a45b494dfe40a83fa4e7877b02
monosodrac <monosodrac@gmail.com>
2026-01-06 18:48:35 -0300

    add new products

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+     function truncateText(className, maxLength) {
+        $(className).each(function() {
+            var text = $(this).text();
+            if (text.length > maxLength) {
+                var truncatedText = text.substring(0, maxLength) + '...';
+                $(this).text(truncatedText);
+            }
+        });
+    }
+
+    truncateText('.desc-produto', 97);
+
+
     $('#telefone').mask('(00) 00000-0000', {
         placeholder: '(__) _____-____'
     })
